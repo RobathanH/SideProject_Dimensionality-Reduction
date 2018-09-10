4a44ff757110af7d86ba962af9d19b6838b27377
RobathanH <rob@igecko.com>
2018-09-09 20:33:41 -0700

    Added wrapper class

--- PCA.py
+++ PCA.py
@@ -8,93 +8,127 @@ MAX_EIGEN_ERROR = 0.0001 #sensitivity of eigenvector measurements. Lower value -
 MAX_EIGEN_ITER = 1000 #number of times the vector is multiplied by the covar matrix before it is given up on and assumed to be impossible
 MIN_COVARIANCE_VALS = 1e-10 #if there are no covariances greater than this value, then all relevant eigenvalues have been found
 
-
-#data should be a DataFrame formatted with each column representing a variable, and each row a separate datapoint
-#data should be normalized
-def createCoVarMtrx(data):
-    testNum, varNum = data.shape
+# The basis class must be created and separately initialized with either setBasisWithCSV(csvFile) or setBasisFromDataframe(panda_data)
+class Basis:
     
-    means = pandas.DataFrame(data.mean(0))
+    def __init__(self):
+        self.initialized = False
+        
+    # INITIALIZING FUNCTIONS
+        
+    # creates and stores a new basis from a csv file
+    def setBasisFromCSV(self, csv_fileName):
+        data = pandas.read_csv(csv_fileName)
+        self.setBasisFromDataframe(data)
     
-    temp = [1] * testNum
+    # creates and stores a new basis from a panda dataframe
+    def setBasisFromDataframe(self, data):
+        covar = self.createCoVarMtrx(data)
 
-    variance = data - numpy.dot(temp, means.transpose())
+        basis = self.findMainEigen(covar)
+	    
+        covar = self.removeEigenComponents(covar, basis)
+	   
+        while (basis.shape[1] < basis.shape[0]) & (covar.max().max() >= MIN_COVARIANCE_VALS):
+            vec = self.findMainEigen(covar)
+            covar = self.removeEigenComponents(covar, vec)
+            basis[basis.shape[1]] = vec
+	   
+        while basis.shape[1] < basis.shape[0]:
+            basis[basis.shape[1]] = [0] * basis.shape[0]
+            
+        self.basis = basis
+        self.initialized = True
+        
     
-    cv = pandas.DataFrame(numpy.dot(variance.transpose(), variance) / (testNum - 1))
-
-    return cv
-
-
-#takes covar matrix, returns column eigenvector, both dataframes
-def findMainEigen(cv):
-    #create arbitrary beginning vector
-    vec = pandas.DataFrame([1] * cv.shape[0])
-    vec /= numpy.sqrt(numpy.square(vec).sum(0))[0] #normalize
-
-    error = MAX_EIGEN_ERROR + 1 #making sure that the while loop runs at least once
-    count = 0
-    while((error > MAX_EIGEN_ERROR) & (count < MAX_EIGEN_ITER)):
-        oldVec = vec
-        vec = pandas.DataFrame(numpy.dot(cv, vec))
-        if vec.sum(0)[0] != 0:
-            vec /= numpy.sqrt(numpy.square(vec).sum(0))[0]
-        error = numpy.sqrt(numpy.square(vec - oldVec).sum(0))[0]
-        count += 1
-
-    if count >= MAX_EIGEN_ITER:
-        print("Critical Error: Eigenvector didn't stabilize.")
-        sys.exit(1)
-
-    return vec
-
-
-#takes covar matrix and column vector, and returns new covar matrix, all dataframes
-def removeEigenComponents(cv, vec):
-    cvCopy = cv
     
-    for col in range(cv.shape[0]):
-        mask = [[]]
-        for i in range(cv.shape[0]):
-            if (i == col):
-                mask[0].append(1)
-            else:
-                mask[0].append(0)
-        mask = pandas.DataFrame(mask)
+    # PROJECTING FUNCTIONS
+        
+    # projects csv into the basis and returns a csv string in the new basis
+    def projectCSV(self, csv_fileName, output_csv_fileName):
+        data = pandas.read_csv(csv_fileName)
+        newData = self.projectData(data)
+        return newData.to_csv(output_csv_fileName)
+        
+        
+    # projects a dataframe into the basis and returns a dataframe in the new basis
+    def projectData(self, data):
+        if not self.initialized:
+            print("Error! No basis has been initialized, so there is nothing to project data onto.")
+            sys.exit(1)
+        
+            
+        newData = pandas.DataFrame()
+
+        for row in range(data.shape[0]):
+            coords = [[]]
+            for col in range(data.shape[1]):
+                coords[0].append(numpy.dot(data.loc[data.index[row], : ], self.basis[col]))
+            newData = newData.append(coords, True)
+
+        return newData        
+        
+        
+        
+        
+        
+        
+    # HELPER FUNCTIONS
+        
+    #data should be a DataFrame formatted with each column representing a variable, and each row a separate datapoint
+    #data should be normalized
+    def createCoVarMtrx(self, data):
+        testNum, varNum = data.shape
+        
+        means = pandas.DataFrame(data.mean(0))
+        
+        temp = [1] * testNum
+        
+        variance = data - numpy.dot(temp, means.transpose())
+        
+        cv = pandas.DataFrame(numpy.dot(variance.transpose(), variance) / (testNum - 1))
+        
+        return cv
+        
+        
+    #takes covar matrix, returns column eigenvector, both dataframes
+    def findMainEigen(self, cv):
+        #create arbitrary beginning vector
+        vec = pandas.DataFrame([1] * cv.shape[0])
+        vec /= numpy.sqrt(numpy.square(vec).sum(0))[0] #normalize
+            
+        error = MAX_EIGEN_ERROR + 1 #making sure that the while loop runs at least once
+        count = 0
+        while((error > MAX_EIGEN_ERROR) & (count < MAX_EIGEN_ITER)):
+            oldVec = vec
+            vec = pandas.DataFrame(numpy.dot(cv, vec))
+            if vec.sum(0)[0] != 0:
+                vec /= numpy.sqrt(numpy.square(vec).sum(0))[0]
+            error = numpy.sqrt(numpy.square(vec - oldVec).sum(0))[0]
+            count += 1
+
+        if count >= MAX_EIGEN_ITER:
+            print("Critical Error: Eigenvector didn't stabilize.")
+            sys.exit(1)
+
+        return vec
+
+
+    #takes covar matrix and column vector, and returns new covar matrix, all dataframes
+    def removeEigenComponents(self, cv, vec):
+        cvCopy = cv
+	    
+        for col in range(cv.shape[0]):
+            mask = [[]]
+            for i in range(cv.shape[0]):
+                if (i == col):
+                    mask[0].append(1)
+                else:
+                    mask[0].append(0)
+            mask = pandas.DataFrame(mask)
 
         parallelComponent = vec * numpy.dot(vec.transpose(), cv[col])[0]
 
         cvCopy = cvCopy - pandas.DataFrame(numpy.dot(parallelComponent, mask))
 
-    return cvCopy
-
-
-def findNewBasis(data):
-    covar = createCoVarMtrx(data)
-
-    basis = findMainEigen(covar)
-    
-    covar = removeEigenComponents(covar, basis)
-   
-    while (basis.shape[1] < basis.shape[0]) & (covar.max().max() >= MIN_COVARIANCE_VALS):
-        vec = findMainEigen(covar)
-        covar = removeEigenComponents(covar, vec)
-        basis[basis.shape[1]] = vec
-   
-    while basis.shape[1] < basis.shape[0]:
-        basis[basis.shape[1]] = [0] * basis.shape[0]
-
-    return basis
-
-
-def projectOntoNewBasis(data, basis):
-    newData = pandas.DataFrame()
-    
-    for row in range(data.shape[0]):
-        coords = [[]]
-        for col in range(data.shape[1]):
-            coords[0].append(numpy.dot(data.loc[data.index[row], : ], basis[col]))
-        newData = newData.append(coords, True)
-
-    return newData
-
-    
+        return cvCopy
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Dimensionality-Reduction
 Experimenting with dimensionality reduction through Principal Component Analysis for semi-correlated data spaces.
+
+The PCA.py script implements a class called Basis, which can analyze a dataset (either from CSV file or from a pandas dataframe) and store a new orthogonal basis in terms of the original basis.
+This new basis will minimize correlation between axes, possibly even with a lower dimensionality in the basis-space.
+
+The Basis class must be initialized separately from its creation, using one of two initializer functions: setBasisFromCSV(csv_fileName) or setBasisFromDataframe(data). These functions will override any previously saved basis.
+There are two ways to project data onto this new basis: projectCSV(csv_fileName, output_csv_fileName) and projectData(data). projectCSV() reads a CSV file and writes to another CSV file. projectData() reads from a pandas dataframe object and returns another dataframe.
+
+TODO: Command-line support, performance optimizations
